205fe4249508ebea7c1b8f03434a5da53b893f41
Frank Pernicano <git@keyboarddevil.com>
2018-09-01 16:17:30 -0400

    Tears
    Tears phase 1, still need to hide a link.

---
+++ tears/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="brewery, leaven brewing, riverview brewery, florida brewery" name="keywords">
+  <meta content="Leaven Brewing - Transforming Grain for the Better" name="description">
+  <!-- Site by Frank - www.KeyboardDevil.com -->
+  <title>Leaven Brewing</title>
+  <link rel="alternate" href="https://www.leavenbrewing.com" hreflang="en-us" />	
+
+  <link href="https://www.leavenbrewing.com/favicon.ico" rel="shortcut icon">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate-css/animate.min.css" rel="stylesheet">
+  <link href="../css/style.css" rel="stylesheet">
+  <link href="tears.css" rel="stylesheet">
+  <script src="../js/instafeed.min.js"></script>
+</head>
+
+<body>
+
+  <div id="Blanco">
+    <h1>Tears for Beers</h1>
+    <p>
+      O Grain! O Hops! O Water! O Yeast!<br/>
+      I miss thy concoction, the joy – it has ceased!<br/>
+      A Hef, A Stout, A Porter, an Ale,<br/>
+      Imbibing your glory has made me a whale.<br/>
+      Pour me a pint, or hell, pour a vat,<br/>
+      This diet is torment, I’d rather be fat.<br/>
+      This life is now pointless, and so full of dread,<br/>
+      Unless thee delivers me something with head.<br/>
+      A bottle, a growler, A nitro on tap,<br/>
+      I’d even take Bud, though I know it is crap.<br/>
+      Domestic, imported, a finely tuned craft,<br/>
+      The variance mocks me, by bottle or draught.<br/>
+      My mood is despondent, I think you’ll agree,<br/>
+      That I need a pour with a strong ABV.<br/>
+      So bitter and sour I stand here today,<br/>
+      Ironically like a great IPA.<br/>
+      So I’m gonna drink, for I know what is right!<br/>
+      Go tap the kegs, and line up a flight.<br/>
+      To err is human, forgiveness divine,<br/>
+      Life is a joke with a sad, empty stein.<br/>
+    </p>
+    <div id="Byline">-Blanco</div>
+  </div>
+
+</body>
+
+</html>
