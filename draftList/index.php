a7505a0e42c519ddecaef2ae477ea978e3b71383
Frank Pernicano <git@keyboarddevil.com>
2019-02-26 16:27:41 -0500

    Update index.php

--- draftList/index.php
+++ draftList/index.php
@@ -1,32 +1,30 @@
-<html>
 <?php
 //header("Content-Type: application/json");
-echo '<p>testing</p>';
+
 require '../cms/beersDB.php';
-$sql = 'SELECT name, abv FROM beers where active=1';
+$sql = 'SELECT name, abv FROM beers WHERE active=1';
 $dbOutput = $conn->query($sql);
+
 if ($dbOutput -> num_rows > 0) {
+  $output = '';
   while($row = $dbOutput ->fetch_assoc()) {
     $dbName = $row["name"];
     $dbAbv = $row["abv"];
-    echo $dbName.'<br>';
+    $output = $output.$dbName.". ";
   }
+  echo $output;
 }
 
 /*
+// format and return JSON
 $json = json_encode($data);
 if ($json === false) {
-    // Avoid echo of empty string (which is invalid JSON), and
-    // JSONify the error message instead:
-    $json = json_encode(array("jsonError", json_last_error_msg()));
-    if ($json === false) {
-        // This should not happen, but we go all the way now:
-        $json = '{"jsonError": "unknown"}';
-    }
-    // Set HTTP response status code to: 500 - Internal Server Error
-    http_response_code(500);
+  $json = json_encode(array("jsonError", json_last_error_msg()));
+  if ($json === false) {
+    $json = '{"jsonError": "unknown"}';
+  }
+  http_response_code(500);
 }
 echo $json;
 */
 ?>
-</html>
