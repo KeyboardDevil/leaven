a0fc71541e9154888a1460ce6381e09bda4e326f
Frank Pernicano <git@keyboarddevil.com>
2019-02-27 20:54:03 -0500

    more testing

--- draftList/index.php
+++ draftList/index.php
@@ -1,22 +1,36 @@
 <?php
-//header("Content-Type: application/json");
+header("Content-Type: application/json");
 
 require '../cms/beersDB.php';
 $sql = 'SELECT name FROM beers WHERE active=1';
 $dbOutput = $conn->query($sql);
-echo '<h2>testing</h2>';
+$jsonString = '';
 if ($dbOutput -> num_rows > 0) {
   $output = '';
   while($row = $dbOutput ->fetch_assoc()) {
     $dbName = $row["name"];
     $output = $output.$dbName.". ";
   }
-  echo $output;
+  //echo $output;
+  // build JSON string
+  $jsonData = '{
+  "HTTP Content-Type Header":"application/json",
+  "titleText":"Leaven Brewing draft list",
+  "uid": "leavenDrafts",
+  "updateDate": "2019-02-25T00:00:00.0Z",
+  "leavenDrafts": [
+    {"mainText": "Frank\'s stout",
+    "abv": "12.1 %"},
+    {"mainText": "Cheeky Monkey",
+    "abv": "8.5 %"},
+    {"mainText": "Adrian loves sours",
+    "abv": "4 %"}],
+  "redirectionUrl": "https://www.leavenbrewing.com"
+  }';
 }
 
-/*
 // format and return JSON
-$json = json_encode($data);
+$json = json_encode($jsonData);
 if ($json === false) {
   $json = json_encode(array("jsonError", json_last_error_msg()));
   if ($json === false) {
@@ -25,5 +39,5 @@ if ($json === false) {
   http_response_code(500);
 }
 echo $json;
-*/
+
 ?>
