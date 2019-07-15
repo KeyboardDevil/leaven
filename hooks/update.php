ad87562e49ff9ddf06b13556a597e6ae3382c92f
Adrian Saldivar <asaldivar@ashleyfurniture.com>
2019-07-15 18:22:02 -0400

    additional logging to update.php

--- hooks/update.php
+++ hooks/update.php
@@ -3,11 +3,12 @@ function updateRepo($LOCAL_REPO)
 {
     // If there is already a repo, just run a git pull to grab the latest changes
     $output = shell_exec("cd {$LOCAL_REPO} && git pull origin master");
-    $status = shell_exec("cd {$LOCAL_REPO} && git rev-parse --short HEAD");
     echo "<br />output: $output";
+    file_put_contents("webhook.log", "\n#############################\ngit pull origin master output:\n$output", FILE_APPEND);
+    $status = shell_exec("cd {$LOCAL_REPO} && git rev-parse --short HEAD");
     echo "<br />status: $status";
     echo "<br />root: $LOCAL_ROOT";
-    file_put_contents("webhook.log", "\n$output\nCurrent hash is $status\n#############################\n", FILE_APPEND);
+    file_put_contents("webhook.log", "\nCurrent hash is $status\n#############################\n", FILE_APPEND);
     die("done " . mktime());
 }
 // Set Variables
