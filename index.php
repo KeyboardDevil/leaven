b76bbbeb2655059f4939a501a044a03e5ec42be4
Frank Pernicano <git@keyboarddevil.com>
2019-08-15 20:59:26 -0400

    background
    another new background photo

--- index.php
+++ index.php
@@ -100,7 +100,7 @@
 
   <?php
     // Random bg image for hero
-    $heroNumber = rand(1,11);
+    $heroNumber = rand(1,12);
     echo '<style>#hero { background: url(img/hero-bg'.$heroNumber.'.jpg) top center fixed;}</style>';
   ?>
 </head>
