d85176aa9652932dd3c342eb6ca29c9470e4370d
Frank Pernicano <git@keyboarddevil.com>
2020-06-17 13:58:57 -0400

    new crowler link

--- crowler/index.php
+++ crowler/index.php
@@ -1,3 +1,3 @@
 <?php
-  echo '<script>window.location.href="https://docs.google.com/forms/d/e/1FAIpQLScnrWBfJrWsqk159zpy_d4NY_lsCaPccPR7Y5bGTTQuaZjbAA/viewform";</script>';
+  echo '<script>window.location.href="https://store.arryved.com/preOrder?locationId=BBORAn6n";</script>';
 ?>
