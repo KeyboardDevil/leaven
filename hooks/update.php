4b8205b03781d09fc05fc3cd9e82184706a031ec
KeyboardDevil <git@keyboarddevil.com>
2021-04-29 20:52:25 -0400

    Mugclub 4
    mugclub 4

--- hooks/update.php
+++ hooks/update.php
@@ -2,7 +2,9 @@
 function updateRepo($LOCAL_REPO)
 {
     // If there is already a repo, just run a git pull to grab the latest changes
-    $output = shell_exec("cd {$LOCAL_REPO} && git pull origin master 2>&1");
+    $output = null
+    $return_val = null
+    exec("cd {$LOCAL_REPO} && git pull origin master 2>&1", $output, $return_val);
     echo "<br />output: $output";
     file_put_contents("webhook.log", "\n#############################\ngit pull origin master output:\n$output", FILE_APPEND);
     $status = shell_exec("cd {$LOCAL_REPO} && git rev-parse --short HEAD 2>&1");
