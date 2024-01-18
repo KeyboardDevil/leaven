3a632bfa207205ccb727c0c7133cebbd3f3cd258
Frank Pernicano <git@keyboarddevil.com>
2024-01-18 15:52:35 -0500

    beer map

--- index.php
+++ index.php
@@ -266,7 +266,7 @@
           ?>
           <h3>Find our Beer NEAR YOU!</h3>
           <p>Looking for Leaven 'in the wild'? Here's where you can find our beer.</p>
-          <iframe width="70%" height="500" src="https://dsdlink.com/aspx1/Home?DashboardID=175197&SupplierID=35116&BrandID=332367" frameborder="0" allowfullscreen  ></iframe>
+          <iframe width="90%" height="500" src="https://dsdlink.com/aspx1/Home?DashboardID=175197&SupplierID=35116&BrandID=332367" frameborder="0" allowfullscreen  ></iframe>
         </div>
       </div>
     </div>
